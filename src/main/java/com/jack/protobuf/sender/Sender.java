0078de2645a3d740b18998a22342f564f17a4459
Jack Hu (juhu2) <juhu2@cisco.com>
2018-07-08 16:48:38 +0800

    change from maven to gradle

---
+++ src/main/java/com/jack/protobuf/sender/Sender.java
@@ -0,0 +1,22 @@
+package com.jack.protobuf.sender;
+
+import com.jack.protobuf.Simple.SimpleMessage;
+
+public class Sender {
+
+	public static void main(String[] args) {
+		SimpleMessage.Builder builder = SimpleMessage.newBuilder();
+		
+		builder.setId(1);
+		builder.setIsSimple(true);
+		builder.setName("This is a test");
+		builder.addSampleList(1);
+		builder.addSampleList(2);
+		System.out.println(builder.toString());
+		
+		SimpleMessage message = builder.build();
+		
+		System.out.println(message.toByteArray());
+	}
+
+}
